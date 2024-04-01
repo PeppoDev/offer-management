cb5307b2c12ae4cf25b16c6187f205128b0d3bb5
Ruan Barros <ruan.victor21052000@gmail.com>
2024-04-01 10:15:07 -0300

    add security configs

---
+++ src/main/java/com/offer/management/offermanagement/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.offer.management.offermanagement.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csfr -> csfr.disable());
+        return http.build();
+    }
+}
